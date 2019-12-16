2142ecf1cbbc9bdc8f3bcbc0f8e84f67231578c7
Darrel Marek <darrelmarek@hotmail.com>
2019-12-16 15:41:40 -0600

    Adds multiplayer game

---
+++ multi/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1576532427,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player_sprite-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/barrier.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
