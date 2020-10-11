0a999b1a75a0b336e5436b395e0cf308991a757c
Darrel Marek <darrelmarek@hotmail.com>
2020-10-11 15:26:25 -0500

    Adds Amongus2

---
+++ amongus2/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1602447924,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/aimspot-sheet0.png",
+		"images/peer-sheet0.png",
+		"images/peer-sheet1.png",
+		"images/peerlaser-sheet0.png",
+		"images/platform-sheet0.png",
+		"images/block-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
