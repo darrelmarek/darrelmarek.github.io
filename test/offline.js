6ad1a5501e2950bc1dbc37520ec03c7336ee452f
Darrel Marek <darrelmarek@hotmail.com>
2020-11-02 13:30:22 -0600

    Adds multiplayer test

---
+++ test/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1604345374,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/peer-sheet0.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
