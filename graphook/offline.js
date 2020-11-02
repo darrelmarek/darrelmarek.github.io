b7d3a5b391ad3bee75c9e16e7168d38d73ab81eb
Darrel Marek <darrelmarek@hotmail.com>
2020-11-02 13:59:19 -0600

    Revert "Updates name of multiplayer test"
    This reverts commit 479e5c28f16467c255712b44b15e3ea59aa71f25.

---
+++ graphook/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1569190416,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/platform-sheet0.png",
+		"images/player-sheet0.png",
+		"images/chain_link-sheet0.png",
+		"images/hook-sheet0.png",
+		"images/smoke.png",
+		"images/particles.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
