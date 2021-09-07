f6ef709d507ec04daaf647f6a3c394e31f7f534e
Darrel Marek <darrelmarek@hotmail.com>
2021-09-07 02:01:58 -0500

    Adds rabbit hunt

---
+++ rabbithunt/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1630998058,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/rabbit-sheet0.png",
+		"images/rabbit-sheet1.png",
+		"images/rabbit-sheet2.png",
+		"images/grass-sheet0.png",
+		"images/rock-sheet0.png",
+		"images/bush-sheet0.png",
+		"images/stick-sheet0.png",
+		"images/fox-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
