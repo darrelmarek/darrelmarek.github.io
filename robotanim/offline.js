3898ff35fed040e8525e00001a6c13ee3f2a94d1
Darrel Marek <darrelmarek@hotmail.com>
2021-10-02 19:53:50 -0500

    Adds robot animation test

---
+++ robotanim/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1633222400,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"images/sprite3-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
