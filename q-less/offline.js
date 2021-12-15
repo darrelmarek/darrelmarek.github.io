fb28e08cf973b468374c88643c7a53c4c187e63b
Darrel Marek <darrelmarek@hotmail.com>
2021-12-14 23:58:51 -0600

    Adds q-less

---
+++ q-less/offline.js
@@ -0,0 +1,16 @@
+﻿{
+	"version": 1639547877,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/block-sheet0.png",
+		"images/background-sheet0.png",
+		"images/shadow-sheet0.png",
+		"media/pickup.m4a",
+		"media/pickup.ogg",
+		"media/putdown.m4a",
+		"media/putdown.ogg"
+	]
+}
